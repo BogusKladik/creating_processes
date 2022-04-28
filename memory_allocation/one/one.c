beda8953f1eca287b0573ffe3c609a17c4f5a372
BogusKladik <boguskladik@gmail.com>
2022-04-28 18:45:41 +0300

    added memory allocation

---
+++ memory_allocation/one/one.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void handler(int sig)
+{
+	switch (sig)
+	{
+		case SIGINT: fprintf(stdout, "Emergency! User death imminent\n"); break;
+		default:;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int number_of_megabytes = atoi(argv[1]);
+
+	signal(SIGINT, handler);
+
+	int *p;
+	while(1) {
+		int inc = number_of_megabytes* 1024 * 1024 * sizeof(char);
+		p=(int*) calloc(1,inc);
+        	if (!p) break;
+    }
+}
